4f3e4108a686ef0ecd87de21e8e19dec7a586209
abmueller2000 <99419714+abmueller2000@users.noreply.github.com>
2023-08-21 13:25:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,39 @@
-// Game variables
-var canvas = document.getElementById('fishingArea');
-var ctx = canvas.getContext('2d');
-var fishes = [];
-var score = 0;
+// Import your classes
+import { Fish } from './fish.js';
+import { FishingLine } from './fishingLine.js';
 
-// Fishing line
-var fishingLine = new FishingLine();
+// Get the canvas and set up the context
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-// Game initialization
-function init() {
-    // Create some initial fish objects
-    for (var i = 0; i < 10; i++) {
-        fishes.push(new Fish());
-    }
+// Create the fishing line
+const fishingLine = new FishingLine();
 
-    // Set up event listeners
-    canvas.addEventListener('mousedown', castLine);
-    canvas.addEventListener('mouseup', reelIn);
+// Event listener for casting or reeling in
+canvas.addEventListener('click', (event) => {
+    fishingLine.cast(event.clientX);
+});
 
-    // Start the game loop
-    requestAnimationFrame(gameLoop);
-}
+// Event listeners for moving the fishing line left and right
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') fishingLine.moveLine('left');
+    if (event.key === 'ArrowRight') fishingLine.moveLine('right');
+});
 
 // Main game loop
 function gameLoop() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw and update the fishing line
-    fishingLine.draw(ctx);
+    // Update and draw the fishing line
     fishingLine.update();
+    fishingLine.draw(ctx);
 
-    // Draw and update the fishes
-    fishes.forEach(fish => {
-        fish.draw(ctx);
-        fish.update();
-    });
+    // Update and draw other game elements like the fish
 
-    // Check for catches
-    checkCatches();
-
-    // Draw the score
-    drawScore();
-
-    // Next frame
+    // Call the game loop again on the next animation frame
     requestAnimationFrame(gameLoop);
 }
 
-// Casting the line
-function castLine(event) {
-    // Logic for casting the line
-    fishingLine.cast(event.clientX, event.clientY);
-}
-
-// Reeling in the line
-function reelIn() {
-    // Logic for reeling in
-    fishingLine.reelIn();
-}
-
-// Check for catches
-function checkCatches() {
-    // Logic to detect catches
-}
-
-// Draw the score
-function drawScore() {
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Arial";
-    ctx.fillText("Score: " + score, 10, 30);
-}
-
-// Start the game
-init();
+// Start the game loop
+gameLoop();
