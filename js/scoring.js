9501d8d6d6c941fdda2af84ece23a9da1db125d9
abmueller2000 <99419714+abmueller2000@users.noreply.github.com>
2023-08-21 13:35:06 -0500

    Update scoring.js

--- js/scoring.js
+++ js/scoring.js
@@ -1,3 +1,15 @@
+// Number of fish in the game
+const numberOfFish = 5;
+
+// Initialize the fishes array
+const fishes = [];
+for (let i = 0; i < numberOfFish; i++) {
+    fishes.push(new Fish());
+}
+
+// Keep track of the player's score
+let score = 0;
+
 // Update the player's score
 function updateScore(points) {
     score += points;
@@ -5,21 +17,22 @@ function updateScore(points) {
 
 // Check for collisions between the fishing line and the fish
 function checkCatches() {
-    fishes.forEach((fish, index) => {
-        if (fishingLine.isCasting && collisionWithLine(fish)) {
+    for (let index = fishes.length - 1; index >= 0; index--) {
+        let fish = fishes[index];
+        if (FishingLine.isCasting && collisionWithLine(fish)) {
             updateScore(10); // Add points for a catch
             fishes.splice(index, 1); // Remove the caught fish
             fishes.push(new Fish()); // Add a new fish
         }
-    });
+    }
 }
 
 // Logic for checking if a fish has collided with the fishing line
 function collisionWithLine(fish) {
     // Adjust these values to fit the actual sizes and positions of your fishing line and fish
-    var lineX = fishingLine.x;
-    var lineY = fishingLine.y;
-    var lineLength = fishingLine.length;
+    var lineX = FishingLine.x;
+    var lineY = FishingLine.y;
+    var lineLength = FishingLine.length;
 
     if (fish.x < lineX + 5 && fish.x + 50 > lineX && 
         fish.y < lineY + lineLength && fish.y + 20 > lineY) {
