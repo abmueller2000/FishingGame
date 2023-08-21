9269dafc2ab5b2077072ed7863879f9d27529b59
abmueller2000 <99419714+abmueller2000@users.noreply.github.com>
2023-08-21 12:55:48 -0500

    Update fish.js

--- js/fish.js
+++ js/fish.js
@@ -4,11 +4,16 @@ class Fish {
         this.y = Math.random() * 600 + 200; // Random starting Y (below the water surface)
         this.speed = Math.random() * 5 + 1; // Random speed
         this.image = new Image();
-        this.image.src = 'https://img.freepik.com/premium-vector/tuna-fish-icon-pixel-art-illustration-8-bit_749131-8.jpg?w=360'; // URL of the fish image
+        
+        // Array of fish images
+        const fishImages = ['images/fish1.png', 'images/fish2.png', 'images/fish3.png'];
+
+        // Randomly pick one of the images
+        this.image.src = fishImages[Math.floor(Math.random() * fishImages.length)];
     }
 
     draw(ctx) {
-        // Draw the fish using the image
+        // Draw the fish using the randomly picked image
         ctx.drawImage(this.image, this.x, this.y, 50, 20);
     }
 
