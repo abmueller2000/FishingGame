9e381696758ecd4d14cfb91b9a0d2c1028d2e7ad
abmueller2000 <99419714+abmueller2000@users.noreply.github.com>
2023-08-21 13:55:51 -0500

    Update FishingLine.js

--- js/FishingLine.js
+++ js/FishingLine.js
@@ -4,31 +4,21 @@ class FishingLine {
         this.y = 0; // Starting Y
         this.length = 200; // Length of the line
         this.isCasting = false;
-        this.velocityY = 2; // Downward velocity
+        this.velocity = 2; // Downward velocity of the line
     }
 
-    cast(clientX) {
-        if (!this.isCasting) {
-            this.isCasting = true;
-            this.x = clientX; // Update the X coordinate based on where the player clicked
-        } else {
-            this.reelIn(); // Reel in on the second click
-        }
+    cast() {
+        // Logic for casting
+        this.isCasting = true;
     }
 
     reelIn() {
+        // Logic for reeling in
         this.isCasting = false;
-        this.y = 0; // Reset the Y coordinate
-    }
-
-    moveLine(direction) {
-        // Move the line left or right
-        const speed = 5;
-        if (direction === "left") this.x -= speed;
-        if (direction === "right") this.x += speed;
     }
 
     draw(ctx) {
+        // Draw the fishing line
         ctx.strokeStyle = "#000";
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
@@ -37,8 +27,14 @@ class FishingLine {
     }
 
     update() {
+        // Move the fishing line down when casting
         if (this.isCasting) {
-            this.y += this.velocityY; // Move down at a constant velocity
+            this.y += this.velocity;
+            if (this.y + this.length > 600) {
+                this.isCasting = false; // Stop when it reaches the bottom
+            }
+        } else {
+            this.y = 0; // Reset the position when not casting
         }
     }
 }
